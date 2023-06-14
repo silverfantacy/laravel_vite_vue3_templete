b7a74d58a1c3d184232dedbced0b7e8f8735c5b2
Zero <silverfantacy@gmail.com>
2023-06-14 16:17:27 +0800

    install flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,10 @@ import router from '@/router'
 
 import pinia from './store'
 
+import { initFlowbite } from 'flowbite'
+
 createApp(App)
     .use(router)
     .use(pinia)
+    .use(initFlowbite)
     .mount("#app")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        "./node_modules/flowbite/**/*.js",
         "./resources/**/*.blade.php",
         "./resources/js/**/*.{vue,js,ts,jsx,tsx}",
     ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin')
+  ],
 }
 
